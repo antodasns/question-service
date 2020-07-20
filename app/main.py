4014e3383ee4ae4ffc5dca8eccf7305ccf6f4c36
antodasns <42759397+antodasns@users.noreply.github.com>
2020-07-20 16:22:28 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route('/question')
 def hello_world():
-    return {
-        "question_description": "",
-        "options": ['A', 'B', 'C', 'D']
-    }
+    return [
+  {
+  "id": "1",
+  "qcategory": "maths",
+  "question": "whjhdkf ghfksh hgkshkfhdk jhvfsk",
+  "correctoption": "A",
+  "options":["hello","hai"]
+},
+  {
+    "id": "2",
+    "qcategory": "physics",
+    "question": "df jhf hlk rerw",
+    "correctoption": "B",
+    "options":["hello","hai"]
+  }
+  ]
